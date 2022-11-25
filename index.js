3447c3f21adea6f3e12e9d9824c3795d3ab87cd4
Tracy <tracy.atieno@student.moringaschool.com>
2022-11-25 15:50:37 +0300

    first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(drivers) {
+     return drivers.slice(0,2)      
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+
+
+function createFareMultiplier(integer) {
+     return function fareMultiplier (multiple) {
+       return integer * multiple
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+ function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers)
+ }
